6fe1b169086428b91c0fd28d46a0f0f6fb9b2224
kenolykarichi <kenolykarichi369@gmail.com>
2022-12-09 23:49:27 -0800

    and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 
-/**main - prints "and of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline, to standard error
- * Return: Alwayss 1.
+/**
+ * main - prints "and that piece of art useful" - Dora Korpar,"2015-10-19",
+ * followed by a newline, to standard error.
+ * Return: Always 1.
  */
 
 int main(void)
 {
-	write(2, "and that peace of art is useful\" -Dora Kopar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
