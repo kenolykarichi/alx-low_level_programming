57adeb3d7f17bd49085c1a189e1ad29e7543d356
kenolykarichi <kenolykarichi369@gmail.com>
2022-12-09 23:32:54 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**main - prints "and of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to standard error
+ * Return: Alwayss 1.
+ */
+
+int main(void)
+{
+	write(2, "and that peace of art is useful\" -Dora Kopar, 2015-10-19\n", 59);
+	return (1);
+}
