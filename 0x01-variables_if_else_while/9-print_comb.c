ca1af8b43e32df1e26e70b193ae48c7b0b2aaacd
kenolykarichi <kenolykarichi369@gmail.com>
2022-12-12 07:56:46 -0800

    9-last

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	int n;
+	int a;
 
-	for (n = 48; n < 58; n++)
+	for (a = 0; a < 10; a++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(a + '0');
+		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		putchar('\n');
-			return (0);
 	}
+	putchar('\n');
+	return (0);
 }
