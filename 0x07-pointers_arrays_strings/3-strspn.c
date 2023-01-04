e88f5fbc4b62e8024c7f4abc4269e17fff7e70ce
kenolykarichi <kenolykarichi369@gmail.com>
2023-01-04 06:59:57 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,15 +10,14 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-
-	for(j = 0; *(s + j); j++)
+	for (j = 0; *(s + j); j++)
 	{
 		for (i = 0; *(accept + i); i++)
 		{
 			if (*(s + j) == *(accept + i))
 				break;
 		}
-		if(*(accept + i) == '\0')
+		if (*(accept + i) == '\0')
 			break;
 	}
 	return (j);
